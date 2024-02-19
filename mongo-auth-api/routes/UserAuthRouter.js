b592134512f98ba3068eee6a2376f7d628466fb9
Jaskaran0702 <137386006+Jaskaran0702@users.noreply.github.com>
2024-02-19 15:34:25 +0530

    Delete mongo-auth-api directory

--- mongo-auth-api/routes/UserAuthRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { signup, login, logout } = require("../controllers/UserAuthController");
-const UserAuthRouter = express.Router();
-
-UserAuthRouter.post("/signup", signup);
-UserAuthRouter.post("/login", login);
-UserAuthRouter.post("/logout", logout);
-
-module.exports = UserAuthRouter;
